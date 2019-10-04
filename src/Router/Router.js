e09ac4474beb665313668362001bbcbfd0f2def8
Cristian1220 <cristianb1220@gmail.com>
2019-10-04 18:16:19 -0500

    vista camioneroa

--- src/Router/Router.js
+++ src/Router/Router.js
@@ -3,16 +3,17 @@ import {BrowserRouter,  Route,  Switch} from 'react-router-dom';
 
 import Home from '../container/Home';
 import Carrier from '../container/Carrier';
-/* import Camionero from '../container/Servicios';
-import Company from '../container/Company'; */
-
+import Camionero from '../container/Servicios';
+import Company from '../container/Company'; 
+ 
 const Router = () => (
     <BrowserRouter>
         <Switch>
             <Route exact path="/" component={Home}/>
             <Route path="/Carrier" component={Carrier}/>
-           {/*  <Route path="/camionero" component={Camionero}/>
-            <Route path="/Company" component={Company}/> */}
+            <Route path="/Camionero" component={Camionero}/>
+            <Route path="/Company" component={Company}/> 
+         
            
         </Switch>
     </BrowserRouter>
